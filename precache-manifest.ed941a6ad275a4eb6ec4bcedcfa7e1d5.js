9fc57833060c3c846fa34c9dc7bf6baac46949e0
Sanja Smrzlic <sanja@shoelace.com>
2019-09-24 19:04:42 -0400

    Updates

---
+++ precache-manifest.ed941a6ad275a4eb6ec4bcedcfa7e1d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81a081d41ea2d12098522279409a876",
+    "url": "/react-sandbox/index.html"
+  },
+  {
+    "revision": "94ab8d97242b314ef27d",
+    "url": "/react-sandbox/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5b241ca531154e96aee7",
+    "url": "/react-sandbox/static/js/2.054d2967.chunk.js"
+  },
+  {
+    "revision": "94ab8d97242b314ef27d",
+    "url": "/react-sandbox/static/js/main.6970f811.chunk.js"
+  },
+  {
+    "revision": "7edb345638af9ea6d520",
+    "url": "/react-sandbox/static/js/runtime~main.821e39cb.js"
+  }
+]);
